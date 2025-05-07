1af682d0be0dc8f986f44deee9415ffef9c28dd7
Neil E. <elkadineil@gmail.com>
2025-05-07 13:13:34 -0400

    Postgresql migration

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,11 @@
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
+export default prisma;
 
 export const prisma = globalForPrisma.prisma || new PrismaClient();
 
