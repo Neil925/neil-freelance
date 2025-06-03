e4970de521260cfd09cf687efe8dd58d6cb80a8e
neil925 <elkadineil@gmail.com>
2025-06-03 15:36:56 -0400

    Fixed reload script

--- reload.sh
+++ reload.sh
@@ -1,6 +1,6 @@
-!#/bin/bash
+#!/bin/bash
 
 pm2 stop freelance
-prisma migrate production || exit 1
+prisma migrate dev --name "$(git log -1 --pretty=%B)" || exit 1
 npm run build || exit 1
 pm2 start freelance
