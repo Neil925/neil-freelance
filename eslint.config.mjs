85aa0ab6740e7104e54d07fe358055cb0a873e93
neil925 <elkadineil@gmail.com>
2025-06-14 16:37:49 -0400

    Allow implicit any

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,11 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ["next"],
+    rules: {
+      "ypescript-eslint/no-explicit-any": "off",
+    },
+  }),
 ];
-
 export default eslintConfig;
