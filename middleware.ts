cb7639b2a8ead8a5ab40dc3b4270044fa3a8d394
neil925 <elkadineil@gmail.com>
2025-06-14 16:46:04 -0400

    Work around base path

--- middleware.ts
+++ middleware.ts
@@ -4,8 +4,14 @@ export async function middleware(req: NextRequest) {
   if (!req.cookies.has("auth_token")) {
     console.log(req.nextUrl);
     const url = req.nextUrl.clone();
+    const originalUrl = req.nextUrl.clone();
+
+    if (process.env.BASE_URL && originalUrl.basePath != process.env.BASE_URL) {
+      originalUrl.basePath = process.env.BASE_URL;
+    }
+
     url.pathname = "/api/auth";
-    url.searchParams.append("returnto", req.nextUrl.toString());
+    url.searchParams.append("returnto", originalUrl.toString());
 
     return NextResponse.redirect(url);
   }
